02c622a96587acc49c80492ac9076120942b73d8
Superopa <evander_ypey@hotmail.com>
2018-12-14 15:25:15 +0100

    Update MyDevices.vue
    Added functions

--- src/components/settings/MyDevices.vue
+++ src/components/settings/MyDevices.vue
@@ -14,14 +14,96 @@
 						<v-layout align-center justify-center wrap row pa-4 ma-3>
 							<h2>{{device.deviceName}}</h2>
 							<v-flex app class="text-xs-right">
-								<v-btn large>Disconnect</v-btn>
+								<v-dialog
+									v-model="dialogDisconnect"
+									width="500"
+									:key="device.id"
+								>
+									<v-btn
+										slot="activator"
+										color="red lighten-2"
+										large
+									>
+										Disconnect
+									</v-btn>
+									<v-card>
+										<v-card-title
+											class="headline primary"
+											primary-title
+										>
+											Confirm action
+										</v-card-title>
+
+										<v-card-text>
+											Are you sure you want to disconnect this device?
+										</v-card-text>
+										<v-card-actions>
+											<v-spacer></v-spacer>
+											<v-btn
+												color="primary"
+												@click="removeDevice(device.id) ; dialogDisconnect = false"
+											>
+												<v-icon>check</v-icon>Yes
+											</v-btn>
+											<v-btn
+												color="red lighten-1"
+												dark
+												@click="dialogDisconnect = false"
+											>
+												<v-icon>close</v-icon>No
+											</v-btn>
+										</v-card-actions>
+									</v-card>
+								</v-dialog>
 							</v-flex>
 						</v-layout>
 					</v-card>
 				</v-flex>
 			</v-flex>
 			<v-layout align-start justify-end row>
-				<v-btn class="right" large>Add Device</v-btn>
+				<v-dialog
+					v-model="dialogConnect"
+					width="500"
+				>
+					<v-btn
+						slot="activator"
+						color="primary"
+						class="right"
+						large
+					>
+						Add Device
+					</v-btn>
+					<v-card>
+						<v-card-title
+							class="headline primary"
+							primary-title
+						>
+							Connect device
+						</v-card-title>
+
+						<v-card-text>
+							<v-text-field label="Enter device name"></v-text-field>
+							<v-text-field label="Enter IP Address"></v-text-field>
+						</v-card-text>
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn
+								activator
+								color="primary"
+								@click="pushNewDevice(Math.random(), 'Test') ; dialogConnect = false"
+							>
+								<v-icon>check</v-icon>Confirm
+							</v-btn>
+							<v-btn
+								color="red lighten-1"
+								dark
+								@click="dialogConnect = false"
+							>
+								<v-icon>close</v-icon>Cancel
+							</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-dialog>
 			</v-layout>
 		</v-container>
 	</v-app>
@@ -47,8 +129,24 @@
 						id: 2,
 						deviceName: "Cloud Server 1"
 					}
-				]
+				],
+				dialogDisconnect: false,
+				dialogConnect: false
 			}
-		}
+		},
+		methods: {
+			pushNewDevice: function (id, deviceName) {
+				this.devices.push({
+					id: id,
+					deviceName: deviceName,
+				})
+			},
+			removeDevice: function (id) {
+				console.log("This is the starter id: "+id)
+				for(var index in this.devices) {
+					console.log(index)
+				}
+			}
+		},
 	}
 </script>
