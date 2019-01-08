7fdc3407ff14ea65b54a2838f1ef7ddc696aff74
YoranYpey <y.e.l.ypey@st.hanze.nl>
2019-01-08 19:48:30 +0100

    Update ConnectionManager.vue

--- src/components/settings/ConnectionManager.vue
+++ src/components/settings/ConnectionManager.vue
@@ -12,21 +12,22 @@
 				<v-flex xs4>
 					<v-card height="500px">
 						<v-flex v-for="i in connections" ma-3 pa-2>
-						<v-list-tile wrap ripple @click="i.selected = true">
+						<v-list-tile wrap ripple @click="checklist(i)">
 							{{ i.serviceId }}
 						</v-list-tile>
 						</v-flex>
 					</v-card>
 				</v-flex>
-				<v-divider vertical></v-divider>
-				<v-flex xs12 sm6>
+				<v-divider vertical ma-3></v-divider>
+				<v-flex wrap xs12 sm6>
 					<v-card row wrap xs12 sm6 height="500px">
-							<v-flex v-for="i in connections" ma-3 pa-2>
+							<v-flex row wrap v-for="i in connections" :key="i.id" ma-3 pa-2>
 								<v-card>
-									<v-card v-if= "i.selected = true">
-											Test
+									<v-card xs12 sm6 wrap row>
+										Test
 										{{ connections[0].selected }}
 										{{ i.notifications }}
+										<v-switch v-model="switch1"></v-switch>
 									</v-card>
 								</v-card>
 							</v-flex>
@@ -48,37 +49,28 @@
 				connections: [
 					{
 						id: 0,
-						serviceId: "Miele wasmachine P039",
-						icon_url: "http://www.bruggink-bv.nl/wp-content/uploads/2015/09/miele-logo-1.png",
-						locations: "Cloud",
-						ip_address: "192.168.1.15",
-						notifications: ["Er is een update beschikbaar.", "Dit is een update"],
-						selected: false
+						serviceId: "Miele wasmachine",
+						selected: false,
+						notifications: ["Er is een update beschikbaar.", "Dit is een update"]
 					},
 					{
 						id: 1,
 						serviceId: "Smart Energy",
-						icon_url: "https://www.davehewer.com/wp-content/uploads/2018/02/greenleaf-logo-branding03.png",
-						locations: "Raspberry Pi - 1",
-						ip_address: "192.168.1.16",
-						notifications: ["Er is een update .", "Dit is een "],
-						selected: false
+						selected: false,
+						notifications: ["Er is een update .", "Dit is een "]
 					},
 					{
 						id: 2,
 						serviceId: "Vue Power",
-						icon_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vue.js_Logo.svg/400px-Vue.js_Logo.svg.png",
-						locations: "Cloud",
-						ip_address: "192.168.1.17",
+						selected: false,
 						notifications: ["Er is een .", "Dit is ate"],
-						selected: false
 					},
 				]
 			}
 	},
 		methods: {
-			checklist: function() {
-				;
+			checklist: function(i) {
+
 			}
 		}
 		}
